633aad60aa1ec134eaedc56eacb4963acbbfa89d
OnurArk <onurark11@gmail.com>
2023-02-15 20:34:19 +0300

    Auth-ContextAPI eklendi

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
+import { AuthContextProvider } from "./context/Auth-Context";
+
 import "./index.css";
 import App from "./App";
 import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <AuthContextProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AuthContextProvider>
 );
