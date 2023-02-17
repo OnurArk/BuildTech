9f0b901226d8d8031ff79b04cf469acc67906367
OnurArk <onurark11@gmail.com>
2023-02-17 17:05:33 +0300

    useValidtation ve refler yerine action kullanıldı

--- src/components/auth/signup/Signup.js
+++ src/components/auth/signup/Signup.js
@@ -1,30 +1,19 @@
-import React, { useRef, useState } from "react";
-import { Form, useNavigation } from "react-router-dom";
+import React from "react";
+import { Form, useActionData, useNavigation } from "react-router-dom";
 
-import useValidation from "../../../hooks/use-validation";
 import Input from "../../ui/Input";
 import Button from "../../ui/Button";
 
 import styled from "./Signup.module.css";
 
 const Signup = () => {
-  const [error, setError] = useState(null);
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
-  const confirmPasswordRef = useRef(null);
-
-  const { signupRequest, isTouched } = useValidation(setError, {
-    email: emailRef,
-    password: passwordRef,
-    confirmPassword: confirmPasswordRef,
-  });
+  const actionData = useActionData();
 
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submittin";
-  // TO DO make this logic in action and use useActionData for error feedback
 
   return (
-    <Form method="post" className={styled.form}>
+    <Form method="post" className={styled.form} noValidate>
       <h1>Sign Up</h1>
       <p>Signup here using your email and password</p>
       <Input
@@ -33,7 +22,6 @@ const Signup = () => {
         placeholder="Example: mail@mai"
         className={styled.input}
         autoComplete="email"
-        ref={emailRef}
       >
         New Email
       </Input>
@@ -43,7 +31,6 @@ const Signup = () => {
         placeholder="Must contain at least 6 characters"
         className={styled.input}
         autoComplete="new-password"
-        ref={passwordRef}
       >
         New Password
       </Input>
@@ -53,7 +40,6 @@ const Signup = () => {
         placeholder="Must contain at least 6 characters"
         className={styled.input}
         autoComplete="new-password"
-        ref={confirmPasswordRef}
       >
         Confirm Password
       </Input>
@@ -65,7 +51,9 @@ const Signup = () => {
           {isSubmitting ? "Submitting..." : "+Signup"}
         </Button>
       </div>
-      {isTouched && error && <p className="err">{error}</p>}
+      {actionData && actionData?.message && (
+        <p className="err">{actionData.message}</p>
+      )}
     </Form>
   );
 };
