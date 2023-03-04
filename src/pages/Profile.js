386203e8cff86c6ff68726223f4f478eb17254bc
OnurArk <onurark11@gmail.com>
2023-03-04 23:52:22 +0300

    profil sayfası oluşturuldu css ile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React from 'react';
+
+import styled from '../styles/Profile.module.css';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className={styled['profile-container']}>
+      <div className={styled['user-container']}>
+        <img
+          src='https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg'
+          alt='user-name'
+          className={styled.img}
+        />
+        <h1>user-name</h1>
+      </div>
+      <div className={styled['info-container']}>
+        <h3 className={styled.titles}>Change Password</h3>
+        <h3 className={styled.titles}>Change User-Name</h3>
+        <h3 className={styled.titles}>Account Details</h3>
+        <h3 className={styled.titles}>Payment Details</h3>
+      </div>
+      <div className={styled.div3}></div>
+    </div>
+  );
 };
 
 export default Profile;
