d98299eab9f7ad403e8d9771349978772f5a55e9
OnurArk <onurark11@gmail.com>
2023-03-09 17:24:49 +0300

    inputs class added as short and long inputs

--- src/components/profile/DynamicPanel/details-navs/Adress.js
+++ src/components/profile/DynamicPanel/details-navs/Adress.js
@@ -30,30 +30,37 @@ const Adress = () => {
         name='line1'
         onChange={handleChange}
         value={address.line1}
-        placeholder='Address Line 1'
+        placeholder='Address Line'
+        className={styled.longAdress}
       />
-      <Input
-        type='text'
-        name='city'
-        onChange={handleChange}
-        value={address.city}
-        placeholder='City'
-      />
-      <Input
-        type='text'
-        name='state'
-        onChange={handleChange}
-        value={address.state}
-        placeholder='State'
-      />
-      <Input
-        type='text'
-        name='country'
-        onChange={handleChange}
-        value={address.country}
-        placeholder='Country'
-      />
-
+      <div className={styled['short-adress-container']}>
+        <Input
+          type='text'
+          name='city'
+          onChange={handleChange}
+          value={address.city}
+          placeholder='City'
+          className={styled.shortAdresses}
+        />
+        <span>:</span>
+        <Input
+          type='text'
+          name='state'
+          onChange={handleChange}
+          value={address.state}
+          placeholder='State'
+          className={styled.shortAdresses}
+        />
+        <span>:</span>
+        <Input
+          type='text'
+          name='country'
+          onChange={handleChange}
+          value={address.country}
+          placeholder='Country'
+          className={styled.shortAdresses}
+        />
+      </div>
       <Button>Save</Button>
     </div>
   );
