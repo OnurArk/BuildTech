26c766196f245e749447a927f9a120cf58a25f69
OnurArk <onurark11@gmail.com>
2023-03-09 17:28:50 +0300

    profile componentına action eklendi

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { redirect } from 'react-router-dom';
+import { updateEmail } from 'firebase/auth';
+import { auth } from '../firebase';
 
 import UserContainer from '../components/profile/User-Container';
 import InfoContainer from '../components/profile/Info-Container';
@@ -19,3 +22,47 @@ const Profile = () => {
 };
 
 export default Profile;
+
+export async function action({ request }) {
+  const toActionData = {};
+  const searchParams = new URL(request.url).searchParams;
+
+  const nav = searchParams.get('nav');
+
+  console.log(nav);
+
+  const data = await request.formData();
+  const phone = data.get('phone');
+  const adress = data.get('adress');
+  const email = data.get('email');
+
+  if (
+    typeof email !== 'string' ||
+    !email.includes('@') ||
+    !email.includes('.com')
+  ) {
+    toActionData.errMessage = 'Email address must contain @ and .com';
+    toActionData.errType
+      ? toActionData.errType.push('email')
+      : (toActionData.errType = ['email']);
+  }
+
+  if (Object.keys(toActionData).length) {
+    return toActionData;
+  }
+
+  if (nav === 'email' && email) {
+    try {
+      await updateEmail(auth.currentUser, email);
+      await auth.signOut();
+      return redirect('/authentication');
+    } catch (err) {
+      err.message = err.message.replace('Firebase: ', '');
+      err.message = err.message.replace(/ *\([^)]*\) */g, '');
+      toActionData.errMessage = err.message;
+      return toActionData;
+    }
+  }
+
+  return redirect('/profile');
+}
