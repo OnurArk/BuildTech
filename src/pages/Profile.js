f7970e2645486b569e385f5f772bcc2e53ebcb3e
OnurArk <onurark11@gmail.com>
2023-03-10 23:43:47 +0300

    firebase cloada user bilgileri atıldı

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { redirect } from 'react-router-dom';
 import { updateEmail } from 'firebase/auth';
-import { auth } from '../firebase';
+import { doc, collection, setDoc, serverTimestamp } from 'firebase/firestore';
+import { auth, db } from '../firebase';
 
 import UserContainer from '../components/profile/User-Container';
 import InfoContainer from '../components/profile/Info-Container';
@@ -25,42 +26,89 @@ export default Profile;
 
 export async function action({ request }) {
   const toActionData = {};
+
+  const user = auth.currentUser;
+  const userInfo = {};
+
+  if (user) {
+    user.providerData.forEach((profile) => {
+      userInfo.uid = profile.uid;
+    });
+  } else {
+    toActionData.errMessage = 'No user Founded';
+  }
+  console.log(user.uid);
+  const userRef = collection(db, 'users');
+
   const searchParams = new URL(request.url).searchParams;
-
   const nav = searchParams.get('nav');
-
-  console.log(nav);
 
   const data = await request.formData();
   const phone = data.get('phone');
-  const adress = data.get('adress');
+
+  // const adress = data.get('adress');
   const email = data.get('email');
 
-  if (
-    typeof email !== 'string' ||
-    !email.includes('@') ||
-    !email.includes('.com')
-  ) {
-    toActionData.errMessage = 'Email address must contain @ and .com';
-    toActionData.errType
-      ? toActionData.errType.push('email')
-      : (toActionData.errType = ['email']);
-  }
+  // Phone Section
 
-  if (Object.keys(toActionData).length) {
-    return toActionData;
-  }
+  if (nav === 'phone') {
+    if (phone?.length < 11) {
+      toActionData.errMessage =
+        'Your phone number at least should have 11 digits!';
+    }
+    if (isNaN(phone)) {
+      toActionData.errMessage = 'That not a number. Please enter number!';
+    }
 
-  if (nav === 'email' && email) {
-    try {
-      await updateEmail(auth.currentUser, email);
-      await auth.signOut();
-      return redirect('/authentication');
-    } catch (err) {
-      err.message = err.message.replace('Firebase: ', '');
-      err.message = err.message.replace(/ *\([^)]*\) */g, '');
-      toActionData.errMessage = err.message;
+    if (Object.keys(toActionData).length) {
       return toActionData;
+    }
+
+    if (phone && phone?.length === 11) {
+      try {
+        await setDoc(doc(userRef, userInfo.uid), {
+          phone: phone,
+          timestamp: serverTimestamp(),
+        });
+        return redirect('?mode=account-details');
+      } catch (err) {
+        err.message = err.message.replace('Firebase: ', '');
+        err.message = err.message.replace(/ *\([^)]*\) */g, '');
+        toActionData.errMessage = err.message;
+        return toActionData;
+      }
+    }
+  }
+
+  // Email Section
+
+  if (nav === 'email') {
+    if (
+      typeof email !== 'string' ||
+      !email.includes('@') ||
+      !email.includes('.com')
+    ) {
+      toActionData.errMessage = 'Email address must contain @ and .com';
+      toActionData.errType
+        ? toActionData.errType.push('email')
+        : (toActionData.errType = ['email']);
+    }
+
+    if (Object.keys(toActionData).length) {
+      return toActionData;
+    }
+
+    if (email) {
+      try {
+        await updateEmail(auth.currentUser, email);
+        await auth.signOut();
+        return redirect('/authentication');
+      } catch (err) {
+        err.message = err.message.replace('Firebase: ', '');
+        err.message = err.message.replace(/ *\([^)]*\) */g, '');
+        toActionData.errMessage = err.message;
+        return toActionData;
+      }
     }
   }
 
