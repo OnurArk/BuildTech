fb88af695811ec3417f280228184ec24a91f7b14
OnurArk <onurark11@gmail.com>
2023-03-10 23:47:02 +0300

    auth contextte user datayı alan bi function added

--- src/context/Auth-Context.js
+++ src/context/Auth-Context.js
@@ -1,32 +1,56 @@
-import React, { useEffect, useState } from 'react';
-
-import { auth } from '../firebase';
+import React, { useCallback, useEffect, useState } from 'react';
+import { query, collection, getDocs } from 'firebase/firestore';
+import { auth, db } from '../firebase';
 
 const AuthContext = React.createContext({
   currentEmail: '',
   currentPhone: '',
+  currentUid: '',
   logout: () => {},
+  getPhone: () => {},
 });
 
 export const AuthContextProvider = (props) => {
   const [currentPhone, setCurrentPhone] = useState();
   const [currentEmail, setCurrentEmail] = useState();
+  const [currentUid, setCurrentUid] = useState();
 
   const logout = () => {
     return auth.signOut();
   };
 
+  const getPhone = useCallback(async () => {
+    try {
+      const userRef = collection(db, 'users');
+      const docRef = query(userRef, currentUid);
+      const docSnap = await getDocs(docRef);
+
+      const mappedData = docSnap.docs.map((doc) => ({
+        id: doc.id,
+        data: doc.data(),
+      }));
+
+      const user = mappedData.find((user) => user.id === currentEmail);
+      if (user) {
+        setCurrentPhone(user?.data.phone);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, [currentEmail, currentUid]);
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
-      console.log(user);
       setCurrentEmail(user?.email);
-      setCurrentPhone(user?.phoneNumber);
+      console.log(user);
+      setCurrentUid(user?.uid);
     });
+    getPhone();
     return unsubscribe;
-  }, []);
+  }, [getPhone]);
 
-  const value = { currentPhone, currentEmail, logout };
-
+  const value = { currentUid, currentPhone, currentEmail, logout, getPhone };
+  console.log(currentPhone);
   return (
     <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>
   );
