64331e81cdedc26de3f401e40a9db114d5cdec5d
OnurArk <onurark11@gmail.com>
2023-03-16 16:15:17 +0300

    hem search paramsla kapanııcak bir user photo sec

--- src/components/profile/user-container/upload-photo/UploadPhoto.js
+++ src/components/profile/user-container/upload-photo/UploadPhoto.js
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
-import { Form } from 'react-router-dom';
+import React from 'react';
+import { Form, Link } from 'react-router-dom';
 
 import Input from '../../../ui/Input';
 import Button from '../../../ui/Button';
 
 import styled from './UploadPhoto.module.css';
 
-function UploadPhoto(props) {
-  const [file, setFile] = useState(null);
-
-  const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-    setFile(selectedFile);
-  };
-
+function UploadPhoto() {
   return (
-    <Form
-      className={styled['upload-photo-container']}
-      onClick={props.toggleEditSec}
-    >
-      <Input type='file' onChange={handleFileChange} />
-      <Button type='submit'>Upload</Button>
-    </Form>
+    <div className={styled.container}>
+      <Link to='/profile' className={styled.background} />
+      <Form
+        method='post'
+        className={styled['upload-photo-container']}
+        encType='multipart/form-data'
+      >
+        <Input name='url-photo' type='text' placeholder='Enter image URL' />
+        <p>or</p>
+        <Input name='local-photo' type='file' />
+        <Button>Upload</Button>
+      </Form>
+    </div>
   );
 }
 
