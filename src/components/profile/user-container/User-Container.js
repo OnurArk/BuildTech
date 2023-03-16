a193d850b7d08b883d67b32004ee50924a5e1dee
OnurArk <onurark11@gmail.com>
2023-03-16 16:14:46 +0300

    artık searchparamsla toggle yapıcak

--- src/components/profile/user-container/User-Container.js
+++ src/components/profile/user-container/User-Container.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import { useActionData } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useActionData, useSearchParams } from 'react-router-dom';
 
 import AuthContext from '../../../context/Auth-Context';
 import UploadPhoto from './upload-photo/UploadPhoto';
@@ -8,28 +8,24 @@ import styled from './User-Container.module.css';
 import { FaEdit } from 'react-icons/fa';
 
 const UserContainer = () => {
-  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [searchParams] = useSearchParams();
+  const isEditOpen = searchParams.get('mode') === 'uploadImg';
 
   const actionData = useActionData();
   const profileCtx = useContext(AuthContext);
-  console.log(actionData);
-
-  const toggleEditSec = () => {
-    setIsEditOpen((pre) => !pre);
-  };
 
   return (
     <div className={styled['user-container']}>
-      {isEditOpen && <UploadPhoto toggleEditSec={toggleEditSec} />}
+      {isEditOpen && <UploadPhoto />}
       <div className={styled['img-container']}>
         <img
           src='https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg'
           alt='user-name'
           className={styled.img}
         />
-        <div className={styled.editPhoto} onClick={toggleEditSec}>
+        <Link to={'?mode=uploadImg'} className={styled.editPhoto}>
           <FaEdit className={styled.icon} />
-        </div>
+        </Link>
       </div>
       <h1>
         {profileCtx.currentUserName ? profileCtx.currentUserName : 'User-Name'}
