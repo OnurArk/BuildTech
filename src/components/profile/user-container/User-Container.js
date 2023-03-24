741026c41052c3fb45cfac9514cd433a6511324f
OnurArk <onurark11@gmail.com>
2023-03-24 23:24:47 +0300

    img yerine backgroun-image kullanıldı

--- src/components/profile/user-container/User-Container.js
+++ src/components/profile/user-container/User-Container.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link, useActionData, useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 import AuthContext from '../../../context/Auth-Context';
 import UploadPhoto from './upload-photo/UploadPhoto';
@@ -7,29 +7,44 @@ import UploadPhoto from './upload-photo/UploadPhoto';
 import styled from './User-Container.module.css';
 import { FaEdit } from 'react-icons/fa';
 
+const noPhoto =
+  'https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg';
+
 const UserContainer = () => {
   const [searchParams] = useSearchParams();
   const isEditOpen = searchParams.get('mode') === 'uploadImg';
 
-  const actionData = useActionData();
   const profileCtx = useContext(AuthContext);
+
+  const photoStyle = {
+    '--urlPhoto': profileCtx?.currentUserPhoto
+      ? `url(${profileCtx?.currentUserPhoto})`
+      : `url(${noPhoto})`,
+    '--position': profileCtx.currentPhotoProp?.position
+      ? profileCtx.currentPhotoProp.position
+      : 'center',
+    '--size': profileCtx.currentPhotoProp?.size
+      ? profileCtx.currentPhotoProp.size
+      : 'cover',
+  };
 
   return (
     <div className={styled['user-container']}>
       {isEditOpen && <UploadPhoto />}
-      <div className={styled['img-container']}>
-        <img
-          src='https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg'
-          alt='user-name'
-          className={styled.img}
-        />
-        <Link to={'?mode=uploadImg'} className={styled.editPhoto}>
-          <FaEdit className={styled.icon} />
-        </Link>
-      </div>
-      <h1>
-        {profileCtx.currentUserName ? profileCtx.currentUserName : 'User-Name'}
-      </h1>
+      {!profileCtx.isLoading && (
+        <div className={styled['img-container']} style={photoStyle}>
+          <Link to={'?mode=uploadImg'} className={styled.editPhoto}>
+            <FaEdit className={styled.icon} />
+          </Link>
+        </div>
+      )}
+      {!profileCtx.isLoading && (
+        <h1>
+          {profileCtx.currentUserName
+            ? profileCtx.currentUserName
+            : 'User-Name'}
+        </h1>
+      )}
     </div>
   );
 };
