ceb4a95560d93a715642e3d7ab588fd77d3dec59
OnurArk <onurark11@gmail.com>
2023-03-29 18:49:28 +0300

    flip card package applied with new functionalities

--- src/components/ui/CreditCard.js
+++ src/components/ui/CreditCard.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactCardFlip from 'react-card-flip';
+
+import AppearanceCreditCard from './AppearanceCreditCard';
 
 import styled from './CreditCard.module.css';
 import { BiCaretRight } from 'react-icons/bi';
@@ -10,6 +13,29 @@ const CreditCard = ({
   numericVal,
   logo,
 }) => {
+  const [isFilipped, setIsFlipped] = useState(false);
+
+  const flipCardHandler = () => {
+    setIsFlipped((pre) => !pre);
+  };
+
+  const flipByEnterHandler = (direction) => {
+    if (direction === 'backCard') {
+      setIsFlipped(true);
+    }
+    if (direction === 'frontCard') {
+      setIsFlipped(false);
+    }
+  };
+
+  useEffect(() => {
+    flipByEnterHandler('backCard');
+  }, [securityVal]);
+
+  useEffect(() => {
+    flipByEnterHandler('frontCard');
+  }, [nameVal, expiryVal, numericVal]);
+
   const containerStyle = {
     '--backgoundCard': backgound.backgoundCard
       ? backgound.backgoundCard
@@ -23,40 +49,66 @@ const CreditCard = ({
   };
 
   return (
-    <div className={styled['credit-card-container']} style={containerStyle}>
-      <img
-        src='https://cdn-icons-png.flaticon.com/512/6404/6404078.png'
-        alt='chip'
-        className={styled.chip}
-      />
+    <ReactCardFlip
+      isFlipped={isFilipped}
+      flipDirection={'horizontal'}
+      flipSpeedFrontToBack={1}
+      flipSpeedBackToFront={1}
+    >
+      <AppearanceCreditCard
+        onClick={flipCardHandler}
+        style={containerStyle}
+        right={'-25%'}
+      >
+        <img
+          src='https://cdn-icons-png.flaticon.com/512/6404/6404078.png'
+          alt='chip'
+          className={styled.chip}
+        />
 
-      <img
-        src={logo}
-        alt='logo'
-        style={{ display: logo ? 'block' : 'none' }}
-        className={styled.logo}
-      />
-      <div className={styled.cardNumber}>
-        <p className={styled.cardText}>card number</p>
-        <h1>{numericVal.replace(/(.{4})/g, '$1 ')}</h1>
-      </div>
-      <div className={styled.nameSection}>
-        <p className={styled.cardText}>cardholder name</p>
-        <h3 className={styled.name}>{nameVal}</h3>
-      </div>
-
-      <div className={styled.expiration}>
-        <p className={styled.cardText}>expiration</p>
-        <div className={styled.date}>
-          <div>
-            <p className={styled.dateText}>VALİD</p>
-            <p className={styled.dateText}>THRU</p>
-          </div>
-          <BiCaretRight />
-          <h3>{expiryVal}</h3>
+        <img
+          src={logo}
+          alt='logo'
+          style={{ display: logo ? 'block' : 'none' }}
+          className={styled.logo}
+        />
+        <div className={styled.cardNumber}>
+          <p className={styled.cardText}>card number</p>
+          <h1>{numericVal.replace(/(.{4})/g, '$1 ')}</h1>
         </div>
-      </div>
-    </div>
+        <div className={styled.nameSection}>
+          <p className={styled.cardText}>cardholder name</p>
+          <h3 className={styled.name}>{nameVal}</h3>
+        </div>
+
+        <div className={styled.expiration}>
+          <p className={styled.cardText}>expiration</p>
+          <div className={styled.date}>
+            <div>
+              <p className={styled.dateText}>VALİD</p>
+              <p className={styled.dateText}>THRU</p>
+            </div>
+            <BiCaretRight />
+            <h3>{expiryVal}</h3>
+          </div>
+        </div>
+      </AppearanceCreditCard>
+
+      <AppearanceCreditCard
+        onClick={flipCardHandler}
+        style={containerStyle}
+        right={'25%'}
+      >
+        <div
+          className={styled.panel}
+          style={{ '--backgoundWave': containerStyle['--backgoundWave'] }}
+        />
+        <div className={styled['sucurity-container']}>
+          <div className={styled['signature-panel']}></div>
+          <div className={styled['security-code']}>{securityVal}</div>
+        </div>
+      </AppearanceCreditCard>
+    </ReactCardFlip>
   );
 };
 
