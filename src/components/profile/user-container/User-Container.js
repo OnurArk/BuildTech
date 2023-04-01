f437ed714c94b6bc45dc441c4a6504ae3c03f08d
OnurArk <onurark11@gmail.com>
2023-04-01 23:22:32 +0300

    Suspense, Await eklendi

--- src/components/profile/user-container/User-Container.js
+++ src/components/profile/user-container/User-Container.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link, useLoaderData, useSearchParams } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { Link, useLoaderData, useSearchParams, Await } from 'react-router-dom';
 
 import UploadPhoto from './upload-photo/UploadPhoto';
 
@@ -10,30 +10,43 @@ const noPhoto =
   'https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg';
 
 const UserContainer = () => {
-  const loaderData = useLoaderData();
+  const { authInfo, photoProp } = useLoaderData();
 
   const [searchParams] = useSearchParams();
   const isEditOpen = searchParams.get('mode') === 'uploadImg';
 
-  const photoStyle = {
-    '--urlPhoto': loaderData?.userPhoto
-      ? `url(${loaderData?.userPhoto})`
-      : `url(${noPhoto})`,
-    '--position': loaderData.photoProp?.position
-      ? loaderData.photoProp.position
-      : 'center',
-    '--size': loaderData.photoProp?.size ? loaderData.photoProp.size : 'cover',
+  const whenLoaded = (authInfo, photoProp) => {
+    const photoStyle = {
+      '--urlPhoto': authInfo?.userPhoto
+        ? `url(${authInfo?.userPhoto})`
+        : `url(${noPhoto})`,
+      '--position': photoProp?.position ? photoProp.position : 'center',
+      '--size': photoProp?.size ? photoProp.size : 'cover',
+    };
+    return photoStyle;
   };
 
   return (
     <div className={styled['user-container']}>
       {isEditOpen && <UploadPhoto />}
-      <div className={styled['img-container']} style={photoStyle}>
-        <Link to={'?mode=uploadImg'} className={styled.editPhoto}>
-          <FaEdit className={styled.icon} />
-        </Link>
-      </div>
-      <h1>{loaderData.userName ? loaderData.userName : 'User-Name'}</h1>
+      <Suspense fallback={<p className='centered'>Loading...</p>}>
+        <Await resolve={{ authInfo, photoProp }}>
+          {(loadedData) => {
+            const { authInfo, photoProp } = loadedData;
+            const photoStyle = whenLoaded(authInfo, photoProp);
+            return (
+              <>
+                <div className={styled['img-container']} style={photoStyle}>
+                  <Link to={'?mode=uploadImg'} className={styled.editPhoto}>
+                    <FaEdit className={styled.icon} />
+                  </Link>
+                </div>
+                <h1>{authInfo.userName ? authInfo.userName : 'User-Name'}</h1>
+              </>
+            );
+          }}
+        </Await>
+      </Suspense>
     </div>
   );
 };
