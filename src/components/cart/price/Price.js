018e0c76400498882af0ccd4c2bded7d2baa1f21
OnurArk <onurark11@gmail.com>
2023-04-17 02:26:21 +0300

    Price is more sensetive to where it is and dynamic

--- src/components/cart/price/Price.js
+++ src/components/cart/price/Price.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import { useSearchParams, Link } from 'react-router-dom';
 
 import Button from '../../ui/Button';
 
 import styled from './Price.module.css';
 
 const Price = () => {
+  const [searchParams] = useSearchParams();
+
+  const mode = searchParams.get('mode');
+
   return (
     <div className={styled['price-container']}>
       <div className={styled.price}>
@@ -12,19 +16,42 @@ const Price = () => {
         <p>10$</p>
       </div>
 
-      <Button
-        className={styled.btn}
-        lineThicness={'3px'}
-        lineLength={45}
-        lineColor={'#DA4919'}
-        blurColor={'#F3B575'}
-        blurSize={4}
-        animationSpeed={3000}
-        rX={20}
-        glowOffset={5}
-      >
-        Continue
-      </Button>
+      {mode !== 'payment' && (
+        <Link
+          to={`?mode=${mode === 'items' ? 'adress' : 'payment'}`}
+          className={styled.link}
+        >
+          <Button
+            className={styled.btn}
+            lineThicness={'3px'}
+            lineLength={45}
+            lineColor={'#DA4919'}
+            blurColor={'#F3B575'}
+            blurSize={4}
+            animationSpeed={3000}
+            rX={20}
+            glowOffset={5}
+          >
+            Continue
+          </Button>
+        </Link>
+      )}
+
+      {mode === 'payment' && (
+        <Button
+          className={styled.btn}
+          lineThicness={'3px'}
+          lineLength={45}
+          lineColor={'#DA4919'}
+          blurColor={'#F3B575'}
+          blurSize={4}
+          animationSpeed={3000}
+          rX={20}
+          glowOffset={5}
+        >
+          Order
+        </Button>
+      )}
     </div>
   );
 };
