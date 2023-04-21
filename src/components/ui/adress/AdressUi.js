8ae85d517357247271f535e5e4c6731ee015c15a
OnurArk <onurark11@gmail.com>
2023-04-21 08:18:51 +0300

    AdressUi transition

--- src/components/ui/adress/AdressUi.js
+++ src/components/ui/adress/AdressUi.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import WithInput from './withInput/WithInput';
 import WithoutInput from './withoutInput/WithoutInput';
+
+import styled from './AdressUi.module.css';
 
 const AdressUi = ({ inCart, toCancel, btnName, isEditOpen, adress }) => {
   const [isEditing, setIsEditing] = useState(isEditOpen || null);
@@ -12,18 +15,38 @@ const AdressUi = ({ inCart, toCancel, btnName, isEditOpen, adress }) => {
 
   return (
     <>
-      {isEditing && (
+      <CSSTransition
+        mountOnEnter
+        unmountOnExit
+        in={isEditing}
+        key={'WithInput'}
+        timeout={{ enter: 500 }}
+        classNames={{
+          enterActive: `${styled.open}`,
+          exitActive: `${styled.close}`,
+        }}
+      >
         <WithInput inCart={inCart} toCancel={toCancel} btnName={btnName} />
-      )}
-      {!isEditing && (
+      </CSSTransition>
+
+      <CSSTransition
+        mountOnEnter
+        unmountOnExit
+        in={!isEditing}
+        key={'WithoutInput'}
+        timeout={{ enter: 500 }}
+        classNames={{
+          enterActive: `${styled.open}`,
+          exitActive: `${styled.close}`,
+        }}
+      >
         <WithoutInput
-          inCart={inCart}
           adress={adress}
           toCancel={toCancel}
           btnName={btnName}
           editHandler={editHandler}
         />
-      )}
+      </CSSTransition>
     </>
   );
 };
