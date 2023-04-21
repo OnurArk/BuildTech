1c7debc908b89d90524224c18adebdf8ba65ef2e
OnurArk <onurark11@gmail.com>
2023-04-21 08:19:35 +0300

    simplifiend for p element

--- src/components/ui/adress/withoutInput/WithoutInput.js
+++ src/components/ui/adress/withoutInput/WithoutInput.js
@@ -3,51 +3,23 @@ import { Link } from 'react-router-dom';
 import Button from '../../Button';
 
 import styled from './WithoutInput.module.css';
-const WithoutInput = ({ inCart, adress, toCancel, btnName, editHandler }) => {
+const WithoutInput = ({ adress, toCancel, btnName, editHandler }) => {
   if (!adress || Object.keys(adress).length === 0) {
     return <h3>Press Edit Button to Add Adress</h3>;
   }
   return (
-    <div
-      className={`${styled['adress-container']} ${
-        inCart ? styled['cart-adress-container'] : null
-      }`}
-    >
-      <p
-        className={`${styled.longAdress} ${
-          inCart ? styled.cartLongAdress : null
-        }`}
-      >
-        {adress.line}
-      </p>
+    <div className={styled['adress-container']}>
+      <p>{adress.line}</p>
       <div className={styled['short-adress-container']}>
-        <p
-          className={`${styled.shortAdresses} ${
-            inCart ? styled.cartShortAdresses : null
-          }`}
-        >
-          {adress.country}
-        </p>
+        <p>{adress.country}</p>
         <span>/</span>
         {adress.state && (
           <>
-            <p
-              className={`${styled.middleAdresses} ${
-                inCart ? styled.cartMiddleAdresses : null
-              }`}
-            >
-              {adress.state}
-            </p>
+            <p>{adress.state}</p>
             <span>/</span>
           </>
         )}
-        <p
-          className={`${styled.shortAdresses} ${
-            inCart ? styled.cartShortAdresses : null
-          }`}
-        >
-          {adress.city}
-        </p>
+        <p>{adress.city}</p>
       </div>
       <div className={styled.buttons}>
         <Link to={toCancel}>
