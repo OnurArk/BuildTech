a6cd49d3d84809c33208148ec142c45c7d8e0cbf
OnurArk <onurark11@gmail.com>
2023-04-26 13:51:18 +0300

    added succesOrder or error mesage

--- src/components/cart/price/Price.js
+++ src/components/cart/price/Price.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSearchParams, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -6,16 +7,29 @@ import Button from '../../ui/Button';
 import styled from './Price.module.css';
 
 const Price = () => {
+  const [isOrdered, setIsOrdered] = useState(false);
+  const [errMsg, setErrMsg] = useState();
+
   const totalPriceCart = useSelector((state) => state.cart.totalPrice);
   const [searchParams] = useSearchParams();
 
   const mode = searchParams.get('mode');
 
+  const orderHandler = () => {
+    console.log('aa');
+    if (totalPriceCart <= 0) {
+      setErrMsg('You Need to Add Items to Cart to Order');
+    } else {
+      setIsOrdered(true);
+      console.log('Ordered Successfully');
+    }
+  };
+
   return (
     <div className={styled['price-container']}>
       <div className={styled.price}>
         <p>Total Price:</p>
-        <p>{`${totalPriceCart} $`}</p>
+        <p>{`${totalPriceCart} TL`}</p>
       </div>
 
       {mode !== 'payment' && (
@@ -50,10 +64,14 @@ const Price = () => {
           animationSpeed={3000}
           rX={20}
           glowOffset={5}
+          onClick={orderHandler}
         >
           Order
         </Button>
       )}
+
+      {errMsg && <p className={styled.errMsg}>{errMsg}</p>}
+      {isOrdered && <p className={styled.succesOrder}>Your Order Is Taken</p>}
     </div>
   );
 };
