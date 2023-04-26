a8318de9a92237183ebd25989c8925e033c5868f
OnurArk <onurark11@gmail.com>
2023-04-26 21:32:59 +0300

    Error Page Styling

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -4,30 +4,38 @@ import { useRouteError } from 'react-router-dom';
 import Header from '../components/layout/header/Header';
 import Footer from '../components/layout/footer/Footer';
 
+import styled from '../styles/ErrorPage.module.css';
+
 const ErrorPage = () => {
   let error = useRouteError();
   console.dir(error);
 
   let title = 'An error occured!';
   let message = 'Could Not Find This Page!';
+  let status = 404;
 
   if (error.status === 404) {
     // message = JSON.parse(error.data).message;
     // If we are not using Responce no need for Json.parse method
     // using react routers json method
+    status = error.status;
     message = error.statusText;
   }
 
   if (error.name === 'FirebaseError') {
     message = error.message;
+    status = 403;
   }
 
   return (
     <>
       <Header />
-      <main>
-        <h1 className='centered err'>{title}</h1>
-        <p className='centered err'>{message}</p>
+      <main className={styled['error-container']}>
+        {(error.status || status) && (
+          <h1 className={styled.statusCode}>{status}</h1>
+        )}
+        <h1 className={styled.title}>{title}</h1>
+        <p className={styled.message}>{message}</p>
       </main>
       <Footer />
     </>
